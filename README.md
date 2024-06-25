5719da8364b846861c31f756884f254caa99c760
kzuu1023 <161953460+Kzuu1023@users.noreply.github.com>
2024-06-25 11:02:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is The Odin Project - Function Assignment
+
+I understand how to create function and the proper way calling a function
+
+This assignment me help me to regain my logic skills and enhance as well.
---
+++ script.js
@@ -0,0 +1,20 @@
+//adding number to 7
+function add7(number) {
+    return number + 7;
+}
+
+console.log(add7(5));
+
+//multiplication
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(5, 4));
+
+//display new string with uppercase first letter
+const str = "hello world";
+
+let result = str.charAt(0).toUpperCase() + str.slice(1);
+
+console.log(result);
